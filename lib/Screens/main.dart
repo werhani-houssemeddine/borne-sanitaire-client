4a7d5647827f4ff5a18f95cc9206f6eb3c701378
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-06 20:32:49 +0100

    Screen Class to hold other screens

---
+++ lib/Screens/main.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: non_constant_identifier_names, library_prefixes
+
+import 'package:flutter/material.dart';
+import 'welcome.dart' as Welcome;
+
+class Screen {
+  static Widget WelcomeWidget() {
+    return const Welcome.WelcomeScreen();
+  }
+}
