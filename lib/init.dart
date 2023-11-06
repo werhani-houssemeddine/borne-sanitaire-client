72512133d2b5a3c34653f35b633a50b428e8ef75
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-06 17:47:32 +0100

    init function for checking if the user is logged in or not

---
+++ lib/init.dart
@@ -0,0 +1,35 @@
+// Check if the user logged In to redirect him to
+// Main Page otherwise he will be redirect to Login Page
+
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart' as path;
+import 'package:borne_sanitaire_client/models/auth_token.dart';
+
+import 'package:borne_sanitaire_client/Server/request.dart';
+
+Future<String> init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final directory = await path.getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+
+  Hive.registerAdapter(AuthTokenAdapter());
+  final Box authBox = await Hive.openBox('authBox');
+  final String? token = await authBox.get('token');
+  if (token != null) {
+    const String endpoint = "/api/client/current-user/";
+    Map<String, String> headers = {"Authorization": token};
+
+    var response = await Request.get(endpoint: endpoint, headers: headers);
+
+    // Must redirect to Login Page and delete the token from authBox
+    if (response.statusCode == 401) {
+      await authBox.delete('token'); // delete token from authBox
+      return "LOGIN";
+    } else {
+      return "HOME";
+    }
+  } else {
+    return "LOGIN";
+  }
+}
