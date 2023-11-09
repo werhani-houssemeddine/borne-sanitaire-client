af843e076f2045c0e4e3a4f19a14d4de1c40be2d
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-09 21:02:20 +0100

    welcome screen

--- lib/Screens/welcome.dart
+++ lib/Screens/welcome.dart
@@ -2,23 +2,20 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 
-import 'package:borne_sanitaire_client/layout/mobile/landing.dart';
+import 'package:borne_sanitaire_client/layout/main.dart' show Landing;
 
 @RoutePage()
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
-  /*if (Platform.isAndroid) {
-    return LandingScreen(context);
-  } else if (Platform.isWindows) {
-
-  } else {
-
-  }
-  */
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LandingScreen(context));
+    if (Platform.isAndroid) {
+      return Scaffold(body: Landing.Mobile(context));
+    } else {
+      // return Scaffold(body: Landing.Windows(context));
+      // return const Text("Under Implementation");
+      return Scaffold(body: Landing.Mobile(context));
+    }
   }
 }
