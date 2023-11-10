8128e6a058070ef3290801366cfb5cb3e6303b5a
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-10 16:53:35 +0100

    add type checking and some verification data type

--- lib/init.dart
+++ lib/init.dart
@@ -1,12 +1,16 @@
 // Check if the user logged In to redirect him to
 // Main Page otherwise he will be redirect to Login Page
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:path_provider/path_provider.dart' as path;
 import 'package:borne_sanitaire_client/models/auth_token.dart';
 
 import 'package:borne_sanitaire_client/Service/request.dart';
+
+import 'package:http/http.dart' as http;
 
 Future<String> init() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,15 +29,24 @@ Future<String> init() async {
       const String endpoint = "/api/client/current-user/";
       Map<String, String> headers = {"Authorization": token};
 
-      var response = await Request.get(endpoint: endpoint, headers: headers);
+      http.Response response =
+          await Request.get(endpoint: endpoint, headers: headers);
 
       // Must redirect to Login Page and delete the token from authBox
       if (response.statusCode == 401) {
         await authBox.delete('token'); // delete token from authBox
         return "LOGIN";
-      } else {
-        return "HOME";
+      } else if (response.statusCode == 200) {
+        var body = jsonDecode(response.body);
+        var state = body['state'];
+        if (state == "SUCCESS") {
+          return "HOME";
+        } else {
+          return "LOGIN";
+        }
       }
+
+      return "LOGIN";
     } else {
       return "LOGIN";
     }
