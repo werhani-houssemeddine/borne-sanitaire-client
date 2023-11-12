f4905fa223fd31f98c43f75ce872babb8c441386
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-12 07:52:48 +0100

    home screen

--- lib/Screens/main.dart
+++ lib/Screens/main.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: non_constant_identifier_names, library_prefixes
 
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'welcome.dart' as Welcome;
 
-class Screen {
-  static Widget WelcomeWidget() {
-    return const Welcome.WelcomeScreen();
+@RoutePage()
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello every one");
   }
 }
