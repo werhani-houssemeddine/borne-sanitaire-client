c9a0df894354e9a46ef4ca8613d94044cffa83e1
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-17 11:15:48 +0100

    perform code optimization change the scope of waitingWidget and initialScreen visibility

--- lib/Screens/initial.dart
+++ lib/Screens/initial.dart
@@ -15,31 +15,46 @@ class InitialScreen extends StatelessWidget {
         home: Scaffold(
             body: FutureBuilder(
       future: init(),
-      builder: initialScreen(),
+      builder: _initialScreen(),
     )));
   }
 }
 
-initialScreen() {
-  return (BuildContext context, AsyncSnapshot<String> snapshot) {
+_initialScreen() {
+  navigateToWelcomePage(BuildContext context) {
+    AutoRouter.of(context).push(const WelcomeRoute()).then((value) => {});
+  }
+
+  navigateToHomePage(BuildContext context) {
+    AutoRouter.of(context).push(const HomeRoute()).then((value) => {});
+  }
+
+  Widget waitingWidget() {
+    return Center(
+      child: LoadingAnimationWidget.staggeredDotsWave(
+        color: Colors.red,
+        size: 50,
+      ),
+    );
+  }
+
+  return (
+    BuildContext context,
+    AsyncSnapshot<INITIALIZATION_RESPONSE> snapshot,
+  ) {
     if (snapshot.connectionState == ConnectionState.done) {
       if (snapshot.hasError) {
         AutoRouter.of(context).push(const WelcomeRoute()).then((value) => {});
       } else if (snapshot.hasData) {
-        AutoRouter.of(context).push(const HomeRoute()).then((value) => {});
+        INITIALIZATION_RESPONSE? data = snapshot.data;
+
+        data == INITIALIZATION_RESPONSE.HOME
+            ? navigateToHomePage(context)
+            : navigateToWelcomePage(context);
       }
       return waitingWidget();
     } else {
       return waitingWidget();
     }
   };
-}
-
-Widget waitingWidget() {
-  return Center(
-    child: LoadingAnimationWidget.staggeredDotsWave(
-      color: Colors.red,
-      size: 50,
-    ),
-  );
 }
