b821ca4c92c5f672db4c979575b5699d90aa2fd5
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-18 19:45:08 +0100

    due to change file location

--- lib/routes/app_router.gr.dart
+++ lib/routes/app_router.gr.dart
@@ -12,7 +12,7 @@ import 'package:auto_route/auto_route.dart' as _i6;
 import 'package:borne_sanitaire_client/Screens/initial.dart' as _i2;
 import 'package:borne_sanitaire_client/Screens/login.dart' as _i3;
 import 'package:borne_sanitaire_client/Screens/main.dart' as _i1;
-import 'package:borne_sanitaire_client/Screens/signup.dart' as _i4;
+import 'package:borne_sanitaire_client/Screens/Signup/main.dart' as _i4;
 import 'package:borne_sanitaire_client/Screens/welcome.dart' as _i5;
 
 abstract class $AppRouter extends _i6.RootStackRouter {
