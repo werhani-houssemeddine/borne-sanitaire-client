b76fa2a8e9d4647daeefdc27bfc39374abcfbe3f
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-19 12:30:09 +0100

    change file location

--- lib/routes/app_router.gr.dart
+++ lib/routes/app_router.gr.dart
@@ -11,7 +11,7 @@
 import 'package:auto_route/auto_route.dart' as _i6;
 import 'package:borne_sanitaire_client/Screens/initial.dart' as _i2;
 import 'package:borne_sanitaire_client/Screens/login.dart' as _i3;
-import 'package:borne_sanitaire_client/Screens/main.dart' as _i1;
+import 'package:borne_sanitaire_client/Screens/Home/main.dart' as _i1;
 import 'package:borne_sanitaire_client/Screens/Signup/main.dart' as _i4;
 import 'package:borne_sanitaire_client/Screens/welcome.dart' as _i5;
 
