6c1e018467097ba69d4077edce9af1d7615c89d8
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-19 19:35:15 +0100

    add try catch

--- lib/Screens/Signup/signup_service.dart
+++ lib/Screens/Signup/signup_service.dart
@@ -7,19 +7,22 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:http/http.dart' as http;
 
 Future<CHECKING_DEVICE> checkDeviceID(String deviceId) async {
-  Map<String, String> queries = {"device_id": deviceId};
-  String endpoint = '/api/client/admin/check-device/';
+  try {
+    Map<String, String> queries = {"device_id": deviceId};
+    String endpoint = '/api/client/admin/check-device/';
 
-  http.Response response =
-      await Request.get(endpoint: endpoint, queries: queries);
+    http.Response response =
+        await Request.get(endpoint: endpoint, queries: queries);
+    if (response.statusCode == 200) {
+      return CHECKING_DEVICE.VALID_DEVICE;
+    } else if (response.statusCode >= 400 && response.statusCode <= 499) {
+      return CHECKING_DEVICE.EXIPRED_DEVICE;
+    }
 
-  if (response.statusCode == 200) {
-    return CHECKING_DEVICE.VALID_DEVICE;
-  } else if (response.statusCode >= 400 && response.statusCode <= 499) {
-    return CHECKING_DEVICE.EXIPRED_DEVICE;
+    return CHECKING_DEVICE.INTERNAL_SERVER_ERROR;
+  } catch (e) {
+    return CHECKING_DEVICE.INTERNAL_SERVER_ERROR;
   }
-
-  return CHECKING_DEVICE.INTERNAL_SERVER_ERROR;
 }
 
 Future<SIGN_UP_RESULT> makeSignUpRequest({
