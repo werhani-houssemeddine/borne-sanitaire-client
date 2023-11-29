2081b932f49ea7dec291f7a69d183f15f1ea18dd
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-29 11:58:44 +0100

    genrated file

--- lib/routes/app_router.gr.dart
+++ lib/routes/app_router.gr.dart
@@ -8,55 +8,119 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i6;
-import 'package:borne_sanitaire_client/Screens/init/initialRoute.dart' as _i2;
-import 'package:borne_sanitaire_client/Screens/login.dart' as _i3;
-import 'package:borne_sanitaire_client/Screens/Home/main.dart' as _i1;
-import 'package:borne_sanitaire_client/Screens/Signup/main.dart' as _i4;
-import 'package:borne_sanitaire_client/Screens/welcome.dart' as _i5;
+import 'package:auto_route/auto_route.dart' as _i9;
+import 'package:borne_sanitaire_client/Screens/Home/main.dart' as _i4;
+import 'package:borne_sanitaire_client/Screens/Home/Screen/agents.dart' as _i1;
+import 'package:borne_sanitaire_client/Screens/Home/Screen/dashboard.dart'
+    as _i2;
+import 'package:borne_sanitaire_client/Screens/Home/Screen/devices.dart' as _i3;
+import 'package:borne_sanitaire_client/Screens/init/initialRoute.dart' as _i5;
+import 'package:borne_sanitaire_client/Screens/login.dart' as _i6;
+import 'package:borne_sanitaire_client/Screens/Signup/main.dart' as _i7;
+import 'package:borne_sanitaire_client/Screens/welcome.dart' as _i8;
 
-abstract class $AppRouter extends _i6.RootStackRouter {
+abstract class $AppRouter extends _i9.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i6.PageFactory> pagesMap = {
-    HomeRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+  final Map<String, _i9.PageFactory> pagesMap = {
+    AgentsRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i1.HomeScreen(),
+        child: const _i1.AgentsScreen(),
+      );
+    },
+    DashboardRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i2.DashboardScreen(),
+      );
+    },
+    DevicesRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i3.DevicesScreen(),
+      );
+    },
+    HomeRoute.name: (routeData) {
+      return _i9.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i4.HomeScreen(),
       );
     },
     InitialRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i2.InitialScreen(),
+        child: const _i5.InitialScreen(),
       );
     },
     LoginRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i3.LoginScreen(),
+        child: const _i6.LoginScreen(),
       );
     },
     SignupRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i4.SignupScreen(),
+        child: const _i7.SignupScreen(),
       );
     },
     WelcomeRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i9.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i5.WelcomeScreen(),
+        child: const _i8.WelcomeScreen(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.HomeScreen]
-class HomeRoute extends _i6.PageRouteInfo<void> {
-  const HomeRoute({List<_i6.PageRouteInfo>? children})
+/// [_i1.AgentsScreen]
+class AgentsRoute extends _i9.PageRouteInfo<void> {
+  const AgentsRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          AgentsRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AgentsRoute';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i2.DashboardScreen]
+class DashboardRoute extends _i9.PageRouteInfo<void> {
+  const DashboardRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          DashboardRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'DashboardRoute';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i3.DevicesScreen]
+class DevicesRoute extends _i9.PageRouteInfo<void> {
+  const DevicesRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          DevicesRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'DevicesRoute';
+
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i4.HomeScreen]
+class HomeRoute extends _i9.PageRouteInfo<void> {
+  const HomeRoute({List<_i9.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -64,13 +128,13 @@ class HomeRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i2.InitialScreen]
-class InitialRoute extends _i6.PageRouteInfo<void> {
-  const InitialRoute({List<_i6.PageRouteInfo>? children})
+/// [_i5.InitialScreen]
+class InitialRoute extends _i9.PageRouteInfo<void> {
+  const InitialRoute({List<_i9.PageRouteInfo>? children})
       : super(
           InitialRoute.name,
           initialChildren: children,
@@ -78,13 +142,13 @@ class InitialRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'InitialRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i3.LoginScreen]
-class LoginRoute extends _i6.PageRouteInfo<void> {
-  const LoginRoute({List<_i6.PageRouteInfo>? children})
+/// [_i6.LoginScreen]
+class LoginRoute extends _i9.PageRouteInfo<void> {
+  const LoginRoute({List<_i9.PageRouteInfo>? children})
       : super(
           LoginRoute.name,
           initialChildren: children,
@@ -92,13 +156,13 @@ class LoginRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'LoginRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i4.SignupScreen]
-class SignupRoute extends _i6.PageRouteInfo<void> {
-  const SignupRoute({List<_i6.PageRouteInfo>? children})
+/// [_i7.SignupScreen]
+class SignupRoute extends _i9.PageRouteInfo<void> {
+  const SignupRoute({List<_i9.PageRouteInfo>? children})
       : super(
           SignupRoute.name,
           initialChildren: children,
@@ -106,13 +170,13 @@ class SignupRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'SignupRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i5.WelcomeScreen]
-class WelcomeRoute extends _i6.PageRouteInfo<void> {
-  const WelcomeRoute({List<_i6.PageRouteInfo>? children})
+/// [_i8.WelcomeScreen]
+class WelcomeRoute extends _i9.PageRouteInfo<void> {
+  const WelcomeRoute({List<_i9.PageRouteInfo>? children})
       : super(
           WelcomeRoute.name,
           initialChildren: children,
@@ -120,5 +184,5 @@ class WelcomeRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'WelcomeRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i9.PageInfo<void> page = _i9.PageInfo<void>(name);
 }
