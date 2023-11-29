063faee36a958f9ebc15927d3bc8f3c4dc571b75
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-29 20:40:52 +0100

    logout

---
+++ lib/Screens/Home/Controller/logout.dart
@@ -0,0 +1,19 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:borne_sanitaire_client/models/auth_token.dart';
+import 'package:borne_sanitaire_client/routes/app_router.gr.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+
+void logout(BuildContext context) async {
+  try {
+    final Box authBox = await Hive.openBox<AuthToken>('authBox');
+    await authBox.clear();
+    await authBox.close();
+
+    if (context.mounted) context.router.replace(const WelcomeRoute());
+  } catch (e) {
+    print("An error occured logout $e");
+
+    if (context.mounted) context.router.replace(const WelcomeRoute());
+  }
+}
