b5177e988d6eeb555ba3567f923e789cd4f8a151
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-29 12:01:02 +0100

    code refactoring

--- lib/Screens/init/init.dart
+++ lib/Screens/init/init.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:borne_sanitaire_client/utils/hive.dart';
 import 'package:borne_sanitaire_client/Service/request.dart';
 import 'package:http/http.dart' as http;
@@ -10,15 +11,18 @@ class AuthManager {
   static Future<INITIALIZATION_RESPONSE> initialize() async {
     try {
       String? token = await _getToken();
-      http.Response response = await _makeRequest(token);
+      if (token != null) {
+        http.Response response = await _makeRequest(token);
 
-      if (response.statusCode == 401) {
-        return _handleFailureRequest();
-      } else if (response.statusCode == 200) {
-        return _handleSuccessRequest(jsonDecode(response.body));
+        if (response.statusCode == HttpStatus.unauthorized) {
+          return _handleFailureRequest();
+        } else if (response.statusCode == HttpStatus.ok) {
+          return _handleSuccessRequest(jsonDecode(response.body));
+        }
+        throw Exception('Unexpected status code: ${response.statusCode}');
+      } else {
+        return INITIALIZATION_RESPONSE.LOGIN;
       }
-
-      throw Exception();
     } catch (e) {
       return INITIALIZATION_RESPONSE.ERROR;
     }
@@ -28,22 +32,22 @@ class AuthManager {
     try {
       LocalStorageAuthToken.registerToken();
       String? token = await LocalStorageAuthToken.getToken();
-
-      if (token != null) return token;
-      throw Exception();
+      return token;
     } catch (e) {
+      print('Error getting token: $e');
       rethrow;
     }
   }
 
   static Future<http.Response> _makeRequest(String? token) async {
     try {
-      if (token == null) throw Exception();
+      if (token == null) throw Exception('Token is null');
 
-      const String endpoint = "/api/client/current-user/";
+      const String endpoint = "/api/client/";
       Map<String, String> headers = {"Authorization": token};
       return await Request.get(endpoint: endpoint, headers: headers);
     } catch (e) {
+      print('Error fetching data: $e');
       rethrow;
     }
   }
