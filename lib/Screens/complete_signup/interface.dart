812cdd9c8571a5c1866acbece95edb9d1ed2f36b
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-07 09:07:44 +0100

    response interface to complete_signup

---
+++ lib/Screens/complete_signup/interface.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: camel_case_types, constant_identifier_names
+
+enum COMPLETE_SIGN_UP_RESPONSE {
+  USED_PHONE_NUMBER,
+  INVALID_PHONE_NUMBER,
+  SUCCESS,
+  INVALID_PROFILE_PICTURE,
+  ERROR,
+  PROFILE_PICTURE_ERROR
+}
