7c89ec47ab4bcdbef23b74de6d64914addf4a030
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-07 09:10:15 +0100

    app bar widget

---
+++ lib/Screens/complete_signup/widget/app_bar.dart
@@ -0,0 +1,56 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:borne_sanitaire_client/routes/app_router.gr.dart';
+import 'package:borne_sanitaire_client/widget/gestor_detector.dart';
+import 'package:flutter/material.dart';
+
+class CompleteUserSignUpAppBar extends StatelessWidget {
+  final bool isDone;
+  final void Function() handleSubmit;
+
+  const CompleteUserSignUpAppBar({
+    Key? key,
+    required this.isDone,
+    required this.handleSubmit,
+  }) : super(key: key);
+
+  MakeGestureDetector makeAppBarLinks({
+    required onPressed,
+    required link,
+    required color,
+  }) {
+    return MakeGestureDetector(
+      onPressed: onPressed,
+      child: Text(
+        link,
+        style: TextStyle(
+          color: color,
+          fontWeight: FontWeight.w700,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          makeAppBarLinks(
+            onPressed: () => AutoRouter.of(context).replace(const HomeRoute()),
+            link: "Skip",
+            color: Colors.black45,
+          ),
+          makeAppBarLinks(
+            onPressed: () => handleSubmit(),
+            link: "done",
+            color: isDone ? Colors.redAccent.shade400 : Colors.black45,
+          ),
+        ],
+      ),
+    );
+  }
+}
