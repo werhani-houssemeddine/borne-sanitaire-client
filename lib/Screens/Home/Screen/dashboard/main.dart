66c40683b5a3c1b2064a940117e1ccbbd6452fe8
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-11 01:46:52 +0100

    make it statefull widget

--- lib/Screens/Home/Screen/dashboard/main.dart
+++ lib/Screens/Home/Screen/dashboard/main.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:borne_sanitaire_client/Screens/Home/Screen/dashboard/calendar.dart';
+import 'package:borne_sanitaire_client/Screens/Home/Screen/dashboard/device.dart';
+import 'package:borne_sanitaire_client/Screens/Home/Screen/dashboard/devices_bar.dart';
 import 'package:borne_sanitaire_client/Screens/Home/Screen/dashboard/statistical.dart';
+import 'package:borne_sanitaire_client/Screens/Home/Service/device.dart';
+import 'package:borne_sanitaire_client/data/device.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -9,14 +13,73 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: initialDataToDahsboard(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data != null) {
+            List<Device> data = snapshot.data;
+            print(data[0].isMainDevice);
+            return DashboardWidgets(listOfDevices: data);
+          }
+        }
+
+        return const Center(
+          child: CircularProgressIndicator.adaptive(),
+        );
+      },
+    );
+  }
+}
+
+class DashboardWidgets extends StatefulWidget {
+  final List<Device> listOfDevices;
+
+  const DashboardWidgets({
+    required this.listOfDevices,
+    super.key,
+  });
+
+  @override
+  State<DashboardWidgets> createState() => _DashboardWidgetsState();
+}
+
+class _DashboardWidgetsState extends State<DashboardWidgets> {
+  Device? currentDevice;
+
+  @override
+  void initState() {
+    for (Device device in widget.listOfDevices) {
+      if (device.isMainDevice == true) {
+        currentDevice = device;
+        break;
+      }
+    }
+    super.initState();
+  }
+
+  void setCurrentDevice(Device device) {
+    setState(() {
+      currentDevice = device;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(20.0),
-      child: const SingleChildScrollView(
+      padding: const EdgeInsets.all(10.0),
+      child: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            CalendarWidget(),
-            Statistical(),
+            const CalendarWidget(),
+            ListOfDevices(
+              devices: widget.listOfDevices,
+              currentDevice: currentDevice!,
+              setState: setCurrentDevice,
+            ),
+            Statistical(device: currentDevice!),
+            DeviceWidget(deviceId: currentDevice!.id),
           ],
         ),
       ),
