755dba3a90aeb006983beda68086f501aa0f48eb
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-11 01:43:05 +0100

    add new color

--- lib/widget/style.dart
+++ lib/widget/style.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color get primary => const Color.fromRGBO(25, 4, 130, 1);
   static Color get bgColor => Colors.blue.shade50;
+  static Color get darkeBg => Colors.blue.shade200;
   static Color get success => const Color.fromRGBO(0, 255, 0, 1);
 }
