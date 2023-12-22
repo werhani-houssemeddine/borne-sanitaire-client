88ff94d9836b7adb98b90e83b31b2e15dd38e161
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-22 11:31:14 +0100

    move file content

--- lib/Screens/Home/Screen/agents/interface.dart
+++ lib/Screens/Home/Screen/agents/interface.dart
@@ -1,2 +1,14 @@
-// ignore: camel_case_types, constant_identifier_names
-enum CURRENT_AGNT_SCREEN { ALL, PENDING, ACTIF, ARCHEIVED }
+// ignore_for_file: camel_case_types, constant_identifier_names
+
+import 'package:borne_sanitaire_client/data/agent.dart';
+
+enum ADD_AGENT_INTERFACE { SUCCESS, EMAIL_USED, MISSING_EMAIL, SERVER_ERROR }
+
+enum GET_ALL_AGENT_INTERFACE { SUCCESS, EMPTY, BAD_REQUEST, SERVER_ERROR }
+
+class AgentListServerResponse {
+  final GET_ALL_AGENT_INTERFACE status;
+  final List<Agent>? data;
+
+  AgentListServerResponse({required this.status, this.data});
+}
--- lib/Screens/Home/interface/agent_inerface.dart
+++
@@ -1,5 +0,0 @@
-// ignore_for_file: camel_case_types, constant_identifier_names
-
-enum ADD_AGENT_INTERFACE { SUCCESS, EMAIL_USED, MISSING_EMAIL, SERVER_ERROR }
-
-enum GET_ALL_AGENT_INTERFACE { SUCCESS, EMPTY, BAD_REQUEST, SERVER_ERROR }
