d04714a1ced8b7b531d879a3530344594e15fae7
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-25 08:41:53 +0100

    add new property to agent and add a new class for pending agent

--- lib/data/agent.dart
+++ lib/data/agent.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:borne_sanitaire_client/utils/convert_date.dart';
+
 class Agent {
   final int Id;
   final String email;
@@ -8,6 +10,7 @@ class Agent {
   final int? phoneNumber;
   final String? profilePicture;
   final List permessions;
+  final String created_at;
 
   Agent({
     required this.Id,
@@ -17,6 +20,7 @@ class Agent {
     required this.permessions,
     required this.phoneNumber,
     required this.profilePicture,
+    required this.created_at,
   });
 
   static Agent makeAgent(Map<dynamic, dynamic> agent_dict) {
@@ -24,7 +28,8 @@ class Agent {
         agent_dict['agent_email'] == null ||
         agent_dict['user_name'] == null ||
         agent_dict['active'] == null ||
-        agent_dict['permessions'] == null) {
+        agent_dict['permessions'] == null ||
+        agent_dict['created_at'] == null) {
       throw ArgumentError('Invalid agent_dict. Missing required fields.');
     }
 
@@ -36,6 +41,35 @@ class Agent {
       phoneNumber: agent_dict['phone_number'],
       profilePicture: agent_dict['profile_picture'],
       permessions: List.from(agent_dict['permessions']),
+      created_at: formatDateTime(agent_dict['created_at']),
+    );
+  }
+}
+
+class PendingAgent {
+  final String Id;
+  final String email;
+  final String created_at;
+
+  static List<PendingAgent> pendingAgents = [];
+
+  PendingAgent({
+    required this.Id,
+    required this.email,
+    required this.created_at,
+  });
+
+  static PendingAgent makePendingAgent(Map<dynamic, dynamic> p_agent_dict) {
+    if (p_agent_dict['id'] == null ||
+        p_agent_dict['agent_email'] == null ||
+        p_agent_dict['created_at'] == null) {
+      throw ArgumentError("Missing required field");
+    }
+
+    return PendingAgent(
+      Id: p_agent_dict['id'],
+      email: p_agent_dict['agent_email'],
+      created_at: formatDateTime(p_agent_dict['created_at']),
     );
   }
 }
